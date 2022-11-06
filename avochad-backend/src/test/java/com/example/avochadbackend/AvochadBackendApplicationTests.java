1057e9471b91073baa1ffce943a1d880f9b5105f
Illia <chippaxyucac@gmail.com>
2022-11-06 15:31:33 +0100

    init backend spring boot

---
+++ avochad-backend/src/test/java/com/example/avochadbackend/AvochadBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.avochadbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvochadBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
