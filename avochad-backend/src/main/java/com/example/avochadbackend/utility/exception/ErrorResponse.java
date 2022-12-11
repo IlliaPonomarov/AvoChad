bf72c7693b2b4b69cd112e76c39cfee507903e7d
Illia <chippaxyucac@gmail.com>
2022-12-11 22:34:35 +0100

    ErrorResponse class was implemented.

---
+++ avochad-backend/src/main/java/com/example/avochadbackend/utility/exception/ErrorResponse.java
@@ -0,0 +1,30 @@
+package com.example.avochadbackend.utility.exception;
+
+public class ErrorResponse {
+    
+    private String message;
+    private Long timestamp;
+
+    public ErrorResponse() {}
+
+    public ErrorResponse(String message, Long timestamp) {
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
